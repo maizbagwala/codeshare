7a6da7d02b245fb09b495791a44e480cc1edb153
maizbagwala <44838294+maizbagwala@users.noreply.github.com>
2023-12-29 20:37:41 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static('public'));
+
+app.get("/",(req,res)=>{
+    res.sendfile(__dirname+"/index2.html");
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+
+  socket.on('update', (code) => {
+    io.emit('update', code);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
